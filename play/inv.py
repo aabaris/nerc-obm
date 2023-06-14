8525c8f3d92fc25d69287ddb4cb4475991cecf87
Augustine Abaris <augustin@bu.edu>
2023-06-14 11:17:49 -0400

    update python parser to loop through output files, print pci devices and disk

--- play/inv.py
+++ play/inv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import os
 
 def debug_rac(rac_json):
 
@@ -20,16 +21,24 @@ def view_rac(rac_json):
     print(system_info['System'][0]['ChassisServiceTag'])
     for pci_dev in system_info['PCIDevice']:
         print("  ",pci_dev['Description'])
+    for disk in system_info['PhysicalDisk']:
+        print("    ",disk['FQDD']," ",disk['Size'])
 
     return 0
 
-rac_file = "../data/racadm-hyp-4.json"
-
-with open(rac_file) as rac_f:
-    rac_data = json.load(rac_f)
+#rac_file = "../data/racadm-hyp-4.json"
+#with open(rac_file) as rac_f:
+#    rac_data = json.load(rac_f)
 
 #print(rac_data)
 #print(json.dumps(rac_data, indent = 4, sort_keys=True))
 
 #a = process_rac(rac_data)
-b = view_rac(rac_data)
+#b = view_rac(rac_data)
+
+for rac_file in os.listdir("../data"):
+    if "racadm-hyp" in rac_file:
+        with open("../data/"+rac_file) as rac_f:
+            rac_data = json.load(rac_f)
+        a = view_rac(rac_data)
+
