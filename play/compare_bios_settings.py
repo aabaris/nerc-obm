4cfd502bcb0cd2c45ab1a57126a7774d870cc2b3
Augustine Abaris <augustin@bu.edu>
2023-06-20 12:54:09 -0400

    clean up unnecesary functions from bios settings comparison script

--- play/compare_bios_settings.py
+++ play/compare_bios_settings.py
@@ -1,36 +1,7 @@
 #!/usr/bin/env python
 
 import json
-import yaml
 import os
-
-class IndentDumper(yaml.Dumper):
-    def increase_indent(self, flow=False, indentless=False):
-        return super(IndentDumper, self).increase_indent(flow, False)
-
-
-def debug_rac(rac_json):
-
-    for key in rac_json['system_info']:
-        print(key,"===") 
-        print(rac_json['system_info'][key])
-
-    return 0
-
-def view_rac(rac_json):
-
-    if bool(rac_json['failed']):
-        print("ERROR: failed racadm call")
-        return 1
-
-    system_info = rac_json['system_info']
-    print(system_info['System'][0]['ChassisServiceTag'])
-    for pci_dev in system_info['PCIDevice']:
-        print("  ",pci_dev['Description'])
-    for disk in system_info['PhysicalDisk']:
-        print("    ",disk['FQDD']," ",disk['Size'])
-
-    return 0
 
 def parse_profile(profile_json):
 
